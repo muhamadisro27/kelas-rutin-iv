9b43a23650869e430801a9175abbb0d12ea79d3e
muhamadisro27 <mohammadisro2710@gmail.com>
2025-10-25 15:02:19 +0700

    feat: add new contract simple plant

---
+++ sesi-1-liskgarden/contracts/05-SimplePlant.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.30;
+
+contract SimplePlant {
+
+    string public plantName;
+    uint256 public waterLevel;
+    bool public isAlive;
+    address public owner;
+    uint256 public plantedTime; 
+
+    constructor() {
+        plantName = "Rose";
+        owner = msg.sender;
+    }
+
+    function water() public  {
+        waterLevel = 100;
+    }
+
+    function getAge() public view returns (uint256) {
+        return block.timestamp - plantedTime;
+    }
+}
